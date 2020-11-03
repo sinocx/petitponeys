4b0bfc9e21cb609f3f37663ebe7f2c09e67320b4
Sinocx <adamsarfat@hotmail.fr>
2020-11-03 02:49:48 +0100

    add choix calendrier : Mois Semaine Jour ListSemaine

--- app/javascript/fullcalendar.js
+++ app/javascript/fullcalendar.js
@@ -9,14 +9,21 @@ document.addEventListener('turbolinks:load', function() {
   var calendarEl = document.getElementById('calendar');
 
   var calendar = new Calendar(calendarEl, {
-    plugins: [ dayGridPlugin, timeGridPlugin, listPlugin],
+    plugins: [ dayGridPlugin,timeGridPlugin,bootstrapPlugin,listPlugin],
+    headerToolbar: {
+          left: 'prev,next today',
+          center: 'title',
+          right: 'dayGridMonth,dayGridWeek,dayGridDay,listWeek'
+        },
+    navLinks: true,
+    eventLimit: true,
     lang: 'fr',
     events: {url: '/cours.json'},
     color: 'yellow',    // an option!
     textColor: 'black',
     height: 900,
     weekends: false,
-     defaultView: 'dayGridWeek'
+    selectable: true,
   });
 
   calendar.render();
