24d811a45f4a2efcf338b5ff2892213dc25d39b0
Sinocx <adamsarfat@hotmail.fr>
2020-11-03 11:50:10 +0100

    changes

--- app/javascript/fullcalendar.js
+++ app/javascript/fullcalendar.js
@@ -2,28 +2,28 @@ import { Calendar } from "@fullcalendar/core";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from '@fullcalendar/timegrid';
 import listPlugin from '@fullcalendar/list';
-import 'bootstrap/dist/css/bootstrap.css';
-import '@fortawesome/fontawesome-free/css/all.css';
-import bootstrapPlugin from '@fullcalendar/bootstrap';
+
 document.addEventListener('turbolinks:load', function() {
   var calendarEl = document.getElementById('calendar');
 
   var calendar = new Calendar(calendarEl, {
-    plugins: [ dayGridPlugin,timeGridPlugin,bootstrapPlugin,listPlugin],
+    
+    plugins: [timeGridPlugin,dayGridPlugin,listPlugin],
+
+    initialView: 'timeGridWeek',
+    timeZone: 'Europe/Paris',
     headerToolbar: {
           left: 'prev,next today',
           center: 'title',
-          right: 'dayGridMonth,dayGridWeek,dayGridDay,listWeek'
+          right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
         },
-    navLinks: true,
-    eventLimit: true,
-    lang: 'fr',
+    locale: 'fr',
     events: {url: '/cours.json'},
-    color: 'yellow',    // an option!
-    textColor: 'black',
+
     height: 900,
     weekends: false,
     selectable: true,
+    editable: true,
   });
 
   calendar.render();
