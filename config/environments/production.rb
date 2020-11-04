0b3c0b4ed1a0c7feb352357a14fd5a7a4600cf31
Sinocx <adamsarfat@hotmail.fr>
2020-11-04 12:57:32 +0100

    asset : https://petitponeys.herokuapp.com

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
+  config.action_controller.asset_host = 'https://petitponeys.herokuapp.com'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
